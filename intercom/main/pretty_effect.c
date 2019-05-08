7ba2b55bcdf522fc37c67ab68bbadb78d0e2df93
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2019-05-09 00:51:08 +0200

    Disable uneeded animation from example

--- intercom/main/pretty_effect.c
+++ intercom/main/pretty_effect.c
@@ -38,7 +38,7 @@ static int8_t xcomp[320], ycomp[240];
 //is displayed; this is used to go to the next frame of animation.
 void pretty_effect_calc_lines(uint16_t *dest, int line, int frame, int linect)
 {
-    if (frame!=prev_frame) {
+    /*if (frame!=prev_frame) {
         //We need to calculate a new set of offset coefficients. Take some random sines as offsets to make everything
         //look pretty and fluid-y.
         for (int x=0; x<320; x++) xofs[x]=sin(frame*0.15+x*0.06)*4;
@@ -46,10 +46,10 @@ void pretty_effect_calc_lines(uint16_t *dest, int line, int frame, int linect)
         for (int x=0; x<320; x++) xcomp[x]=sin(frame*0.11+x*0.12)*4;
         for (int y=0; y<240; y++) ycomp[y]=sin(frame*0.07+y*0.15)*4;
         prev_frame=frame;
-    }
+    }*/
     for (int y=line; y<line+linect; y++) {
         for (int x=0; x<320; x++) {
-            *dest++=get_bgnd_pixel(x+yofs[y]+xcomp[x], y+xofs[x]+ycomp[y]);
+            *dest++=get_bgnd_pixel(x, y);
         }
     }
 }
